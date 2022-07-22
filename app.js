b2ca146020ecf5a858295d7eb9eaea4f60f773ad
Eduard <>
2022-07-21 19:37:06 -0500

    JS changes: change draw color

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const container = document.getElementById("container");
+const container = document.getElementById("container_box");
+let slider = document.getElementById("grid_value");
+const buttons = document.querySelectorAll(".color_button");
+
+let color; // saves color.
+
+
+//Change color.
+buttons.forEach(function(color_button){
+    color_button.addEventListener("click", function(e){
+        const classChecker = e.currentTarget.classList;
+        console.log(classChecker)
+        if(classChecker.contains("black")){
+           color = "black"
+        }else if(classChecker.contains("random")){
+            color = "red"
+        }       
+    })
+})
 
 function createGrid(x){
     for(let i=0; i<x*x; i++){
@@ -7,22 +25,22 @@ function createGrid(x){
         container.style.gridTemplateRows = `repeat(${x}, 1fr)`
         container.appendChild(cells).classList.add("box");
     }
-}
-
-createGrid(16)
-const boxes = document.querySelectorAll(".box");
-
-
-function draw(){
-    boxes.forEach(function(box){
+    document.querySelectorAll(".box").forEach(function(box){
         box.addEventListener('mouseover', function(){
-          box.style.backgroundColor = 'black';  
+          box.style.backgroundColor = color;  
+          console.log(color)
         })
     })
 }
+createGrid(2)
 
 
-draw()
+function changeGrid(){
+    let box = container.querySelectorAll(".box");
+    box.forEach(boxes => boxes.remove());
+    createGrid(slider.value);
+}
+
 
 
 
